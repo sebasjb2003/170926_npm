1f25198e1ee65186e47749d58815f515bd36ec04
sebasjb2003 <sebasjb2003@gmail.com>
2026-09-17 03:39:16 +0200

    Tarea01 AXIOS y MOMENT

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("Hola")
+//moment
+moment.locale("es");
+const fecha = moment();
+
+const texto = "Bienvenido a la consola, hoy estamos a " + fecha.format("dddd DD/MM/YYYY");
+
+console.log(texto);;
+
+//axios
+
+const selector = document.getElementById("selectorUsuario");
+const usuario = document.getElementById("usuario");
+
+// Obtener usuarios con Axios
+axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(function (response) {
+
+        const usuarios = response.data;
+
+        usuarios.forEach(function (persona) {
+
+            const option = document.createElement("option");
+
+            option.value = persona.id;
+            option.textContent = persona.name;
+
+            selector.appendChild(option);
+        });
+    })
+    .catch(function (error) {
+        console.log("Error:", error);
+    });
